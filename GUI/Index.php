56f4ab999f8217b855656c93f7dcd76174eaf3bf
Nanito <norlanalguera@gmail.com>
2022-05-01 10:31:38 -0600

    Estructura HTML Base (sin estilos)

--- GUI/Index.php
+++ GUI/Index.php
@@ -1,1 +1,25 @@
+<?php
+    //Librerias Necesarias
+?>
 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>
+        ???
+    </title>
+    <!--Enlace de Bootstrap 4.6.1-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+    <body>
+        Content
+
+        <footer>
+
+        </footer>
+
+        <!--Enlace JQuery 3.4.1 y JS de Bootstrap-->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    </body>
+</html>
