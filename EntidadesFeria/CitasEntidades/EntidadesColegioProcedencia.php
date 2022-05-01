aef12c5db327c505cfe74c980c6886e9d2d996fc
Nanito <norlanalguera@gmail.com>
2022-05-01 11:18:24 -0600

    Estructura HTML Base (sin estilos)

--- EntidadesFeria/CitasEntidades/EntidadesColegioProcedencia.php
+++ EntidadesFeria/CitasEntidades/EntidadesColegioProcedencia.php
@@ -2,6 +2,54 @@
     class ColegioProcedencia
     {
         private $id;
+        private $nombre;
         private $active;
-        //¡¡¡PENDIENTE!!!
+
+        /**
+         * @return mixed
+         */
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        /**
+         * @param mixed $id
+         */
+        public function setId($id)
+        {
+            $this->id = $id;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getNombre()
+        {
+            return $this->nombre;
+        }
+
+        /**
+         * @param mixed $nombre
+         */
+        public function setNombre($nombre)
+        {
+            $this->nombre = $nombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getActive()
+        {
+            return $this->active;
+        }
+
+        /**
+         * @param mixed $active
+         */
+        public function setActive($active)
+        {
+            $this->active = $active;
+        }
     }
