b5fcba0b908a0a97ccdc35baf5f7020efead4f77
Nanito <norlanalguera@gmail.com>
2022-05-04 16:01:47 -0600

    PerfilesTodos

--- BL/Usuario/BuscarTodosUsuario.php
+++ BL/Usuario/BuscarTodosUsuario.php
@@ -2,14 +2,16 @@
     include '../../Core/Conexion.php';
     include '../../DAL/UsuarioDAL/DALUsuario.php';
     include '../../DAL/UsuarioDAL/DALCredenciales.php';
+    include '../../DAL/UsuarioDAL/DALPerfiles.php';
     include '../../Entidades/UsuarioEntidades/Usuario.php';
     include '../../Entidades/UsuarioEntidades/Credenciales.php';
 
     $UsuarioDAL = new DALUsuario();
     $CredencialesDAL = new DALCredenciales();
+    $perfilesDAL = new DALPerfiles();
 
     $todosUsuarios = $UsuarioDAL->BuscarTodosUsuario();
     $todasCredenciales = $CredencialesDAL->BuscarTodasCredenciales();
-
+    $todosPerfiles = $perfilesDAL->BuscarTodosPerfiles();
 ////////////////////////////////////////////////////////////////////////////////
 
