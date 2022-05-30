e4ed2bac77047e5c6ceb66d6eeedfb6401368e75
Nanito <norlanalguera@gmail.com>
2022-05-30 14:48:01 -0600

    qepdim

---
+++ BL/Usuario/NuevaCredencial.php
@@ -0,0 +1,25 @@
+<?php
+    include '../../Core/Conexion.php';
+    include '../../DAL/UsuarioDAL/DALUsuario.php';
+    include '../../Entidades/UsuarioEntidades/Credenciales.php';
+
+    //EN PROCESO DE DESARROLLO...
+
+    // $nuevaCredencial = new Credenciales();
+    // $credencialDAL = new DALCredenciales();
+
+    // //OBTENCIÓN DE DATOS DESDE EL FRONT
+    // $nuevaCredencial->setCorreo($_POST['']);
+    // $nuevaCredencial->getContrasena($_POST['']);
+
+    // //Nueva Credencial
+    // if($credencialDAL->NuevaCredencial($nuevaCredencial))
+    // {
+    //     //Redireccionamiento
+    // }
+    // else
+    // {
+    //     //Redireccionamiento
+    // }
+
+///////////////////////////////////////////////////////////////////////////////////////////
